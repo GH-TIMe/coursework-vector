9a1f9e7ba3c4ff9d8d60edb276faa8ba1039f1ba
TIMe <timhaha@yandex.ru>
2020-12-08 16:20:32 +0300

    Updates

---
+++ precache-manifest.ecf7a7b2c8cb0a38e48f9cf3be68f46c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d27c4cc0ecdcda9f22aa044d36e001c",
+    "url": "/coursework-vector/index.html"
+  },
+  {
+    "revision": "00ed82b13567f4564c73",
+    "url": "/coursework-vector/static/js/2.56dec742.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/coursework-vector/static/js/2.56dec742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bafdf67b4d10823fb5b",
+    "url": "/coursework-vector/static/js/main.2eb614dd.chunk.js"
+  },
+  {
+    "revision": "73b91a4d49ecc1cd0f0c",
+    "url": "/coursework-vector/static/js/runtime-main.87d6952f.js"
+  },
+  {
+    "revision": "0330955c25a4bc7a19dc7f55d485ed4c",
+    "url": "/coursework-vector/static/media/calculate-black-24dp.0330955c.svg"
+  },
+  {
+    "revision": "192f3bb60ba40d21170a7c1cecca8ea5",
+    "url": "/coursework-vector/static/media/construction-black-24dp.192f3bb6.svg"
+  }
+]);
